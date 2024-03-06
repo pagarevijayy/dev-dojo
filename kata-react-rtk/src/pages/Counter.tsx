989c09111114199ee9eb283881e63c540e7cb9fe
pagarevijayy <pagarevj97@gmail.com>
2024-03-06 12:01:35 +0530

    Redux: counter

--- kata-react-rtk/src/pages/Counter.tsx
+++ kata-react-rtk/src/pages/Counter.tsx
@@ -1,4 +1,30 @@
-const Counter = () => {
-    return <div>Counter</div>;
-};
-export default Counter;
+import type { RootState } from "../state/store";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../state/counter/counterSlice";
+
+export default function Counter() {
+    const count = useSelector((state: RootState) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="space-y-4">
+            <p>Counter Value: {count}</p>
+            <div>
+                <button
+                    className="btn-primary"
+                    aria-label="Increment value"
+                    onClick={() => dispatch(increment())}
+                >
+                    Increment
+                </button>
+                <button
+                    className="btn-primary"
+                    aria-label="Decrement value"
+                    onClick={() => dispatch(decrement())}
+                >
+                    Decrement
+                </button>
+            </div>
+        </div>
+    );
+}
