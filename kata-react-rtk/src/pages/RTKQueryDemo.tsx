102e98874f1cf0839cd99afef709bd7ae9188d08
pagarevijayy <pagarevj97@gmail.com>
2024-03-06 13:05:54 +0530

    basic RTK query

--- kata-react-rtk/src/pages/RTKQueryDemo.tsx
+++ kata-react-rtk/src/pages/RTKQueryDemo.tsx
@@ -1,4 +1,29 @@
+import { useGetPokemonByNameQuery } from "../services/api/pokemon";
+
 const RTKQueryDemo = () => {
-    return <div>RTKQueryDemo</div>;
+    const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+
+    return (
+        <div className="space-y-4">
+            <p>Fetched Pokemon Details:</p>
+            <div>
+                {error ? (
+                    <>Oh no, there was an error</>
+                ) : isLoading ? (
+                    <>Loading...</>
+                ) : data ? (
+                    <div className="flex gap-4 text-center">
+                        <section className="border rounded-lg py-2 px-6">
+                            <img
+                                src={data.sprites.front_shiny}
+                                alt={data.species.name}
+                            />
+                            <h3 className="">{data.species.name}</h3>
+                        </section>
+                    </div>
+                ) : null}
+            </div>
+        </div>
+    );
 };
 export default RTKQueryDemo;
--- kata-react-rtk/src/state/store.ts
+++ kata-react-rtk/src/state/store.ts
@@ -2,14 +2,21 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './counter/counterSlice'
 import watchingReducer from './watching/watchingSlice'
 
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { pokemonApi } from '../services/api/pokemon'
+
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-        watching: watchingReducer
+        watching: watchingReducer,
+        [pokemonApi.reducerPath]: pokemonApi.reducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(pokemonApi.middleware),
 })
+
+setupListeners(store.dispatch)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
